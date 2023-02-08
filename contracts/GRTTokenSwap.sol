3915d9882b313a3ae98d1d22f6949d013c026652
Tomás Migone <tomas@edgeandnode.com>
2023-02-08 10:33:04 -0300

    fix: rename some events
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- contracts/GRTTokenSwap.sol
+++ contracts/GRTTokenSwap.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title GRTTokenSwap
 /// @notice A token swap contract that allows exchanging tokens minted by Arbitrum's standard GRT contract for the canonical GRT token
@@ -24,17 +24,22 @@ contract GRTTokenSwap is Ownable {
     // -- Errors --
     /// @dev Cannot process 0 tokens amounts
     error AmountMustBeGreaterThanZero();
-    /// @dev Canonical and standard token addresses can't be the same
-    error TokensCannotMatch();
-    /// @dev The token is not supported by this contract
-    error InvalidToken();
+    /// @dev Canonical and standard pair addresses are invalid. Either the same or one is 0x00
+    error InvalidTokenAddressPair();
+    /// @dev Token address not supported by this contract
+    error InvalidTokenAddress();
     /// @dev The contract does not have enough canonical GRT tokens to cover the swap
     error ContractOutOfFunds();
 
     // -- Functions --
     /// @notice The constructor for the GRTTokenSwap contract
     constructor(IERC20 _canonicalGRT, IERC20 _standardGRT) {
-        if (_canonicalGRT == _standardGRT) revert TokensCannotMatch();
+        if (
+            address(_canonicalGRT) == address(0) ||
+            address(_standardGRT) == address(0) ||
+            address(_canonicalGRT) == address(_standardGRT)
+        ) revert InvalidTokenAddressPair();
+
         canonicalGRT = _canonicalGRT;
         standardGRT = _standardGRT;
     }
@@ -56,15 +61,14 @@ contract GRTTokenSwap is Ownable {
 
     /// @notice Get the token balances
     /// @return Balance of canonicalGRT and standardGRT
-    function getTokenBalances() external view returns (uint256, uint256) {
+    function getTokenBalances() public view returns (uint256, uint256) {
         return (canonicalGRT.balanceOf(address(this)), standardGRT.balanceOf(address(this)));
     }
 
     /// @notice Transfer all tokens to the contract owner
     /// @dev This is a convenience function to clean up after the contract it's deemed to be no longer necessary
     function sweep() external onlyOwner {
-        uint256 canonicalBalance = canonicalGRT.balanceOf(address(this));
-        uint256 standardBalance = standardGRT.balanceOf(address(this));
+        (uint canonicalBalance, uint standardBalance) = getTokenBalances();
         takeCanonical(canonicalBalance);
         takeStandard(standardBalance);
     }
@@ -86,7 +90,7 @@ contract GRTTokenSwap is Ownable {
     /// @param _amount The amount of tokens to take
     function _take(IERC20 _token, uint256 _amount) private {
         if (_amount == 0) revert AmountMustBeGreaterThanZero();
-        if (_token != canonicalGRT && _token != standardGRT) revert InvalidToken();
+        if (_token != canonicalGRT && _token != standardGRT) revert InvalidTokenAddress();
         _token.transfer(owner(), _amount);
 
         emit TokensTaken(msg.sender, address(_token), _amount);
