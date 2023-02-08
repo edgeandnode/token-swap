10969ab3694388ffa9aa2ab5066df7ef37a5f5cc
Tomás Migone <tomas@edgeandnode.com>
2023-02-08 10:26:19 -0300

    feat: use oz ownable
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- contracts/GRTTokenSwap.sol
+++ contracts/GRTTokenSwap.sol
@@ -3,54 +3,40 @@
 pragma solidity ^0.8.0;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title GRTTokenSwap
 /// @notice A token swap contract that allows exchanging tokens minted by Arbitrum's standard GRT contract for the canonical GRT token
 /// @notice Note that the inverse swap is not supported
 /// @dev This contract needs to be topped off with enough canonical GRT to cover the swaps
-contract GRTTokenSwap {
+contract GRTTokenSwap is Ownable {
     // -- State --
 
     /// The GRT token contract using the custom GRT gateway
     IERC20 public immutable canonicalGRT;
-
     /// The GRT token contract using Arbitrum's standard ERC20 gateway
     IERC20 public immutable standardGRT;
 
-    /// The owner of the contract
-    address public owner;
-
     // -- Events --
-    event OwnerSet(address indexed owner);
+    event TokensSwapped(address indexed user, uint256 amount);
+    event TokensTaken(address indexed owner, address indexed token, uint256 amount);
 
     // -- Errors --
+    /// @dev Cannot process 0 tokens amounts
     error AmountMustBeGreaterThanZero();
-    error OwnerCannotBeZeroAddress();
-    error SenderNotAuthorized();
-
     /// @dev Canonical and standard token addresses can't be the same
     error TokensCannotMatch();
-
     /// @dev The token is not supported by this contract
     error InvalidToken();
-
     /// @dev The contract does not have enough canonical GRT tokens to cover the swap
     error ContractOutOfFunds();
 
-    // -- Modifiers --
-
-    modifier onlyOwner() {
-        if (msg.sender != owner) revert SenderNotAuthorized();
-        _;
-    }
-
     // -- Functions --
-
-    constructor(address _owner, IERC20 _canonicalGRT, IERC20 _standardGRT) {
+    /// @notice The constructor for the GRTTokenSwap contract
+    constructor(IERC20 _canonicalGRT, IERC20 _standardGRT) {
         if (_canonicalGRT == _standardGRT) revert TokensCannotMatch();
         canonicalGRT = _canonicalGRT;
         standardGRT = _standardGRT;
-        _setOwner(_owner);
     }
 
     /// @notice Swap standard GRT for canonical GRT
@@ -64,6 +50,8 @@ contract GRTTokenSwap {
 
         standardGRT.transferFrom(msg.sender, address(this), _amount);
         canonicalGRT.transfer(msg.sender, _amount);
+
+        emit TokensSwapped(msg.sender, _amount);
     }
 
     /// @notice Get the token balances
@@ -73,32 +61,34 @@ contract GRTTokenSwap {
     }
 
     /// @notice Transfer all tokens to the contract owner
-    /// @dev This is a convenience function to clean up after the contract it's deem to be no longer necessary
+    /// @dev This is a convenience function to clean up after the contract it's deemed to be no longer necessary
     function sweep() external onlyOwner {
-        take(canonicalGRT, canonicalGRT.balanceOf(address(this)));
-        take(standardGRT, standardGRT.balanceOf(address(this)));
+        uint256 canonicalBalance = canonicalGRT.balanceOf(address(this));
+        uint256 standardBalance = standardGRT.balanceOf(address(this));
+        takeCanonical(canonicalBalance);
+        takeStandard(standardBalance);
     }
 
-    /// @notice Take tokens from the contract
+    /// @notice Take standard tokens from the contract and send it to the owner
+    /// @param _amount The amount of tokens to take
+    function takeStandard(uint256 _amount) public onlyOwner {
+        _take(standardGRT, _amount);
+    }
+
+    /// @notice Take canonical tokens from the contract and send it to the owner
+    /// @param _amount The amount of tokens to take
+    function takeCanonical(uint256 _amount) public onlyOwner {
+        _take(canonicalGRT, _amount);
+    }
+
+    /// @notice Take tokens from the contract and send it to the owner
     /// @param _token The token to take
     /// @param _amount The amount of tokens to take
-    function take(IERC20 _token, uint256 _amount) public onlyOwner {
+    function _take(IERC20 _token, uint256 _amount) private {
         if (_amount == 0) revert AmountMustBeGreaterThanZero();
         if (_token != canonicalGRT && _token != standardGRT) revert InvalidToken();
-        _token.transfer(owner, _amount);
-    }
+        _token.transfer(owner(), _amount);
 
-    /// @notice Set the owner of the contract
-    /// @param _owner The address of the new owner
-    function setOwner(address _owner) external onlyOwner {
-        _setOwner(_owner);
-    }
-
-    /// @dev Set the owner of the contract
-    /// @param _owner The address of the new owner
-    function _setOwner(address _owner) private {
-        if (_owner == address(0)) revert OwnerCannotBeZeroAddress();
-        owner = _owner;
-        emit OwnerSet(owner);
+        emit TokensTaken(msg.sender, address(_token), _amount);
     }
 }
